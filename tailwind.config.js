329bb61f937f20a1a29ef3815112eb2bf957b537
VidyaKadam2 <124873586+VidyaKadam2@users.noreply.github.com>
2023-03-24 12:57:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: theme =>({
+        '1-img':"url(/img/1.png)",
+        '2-img':"url(/img/2.png)",
+        '3-img':"url(/img/3.png)",
+        '4-img':"url(/img/4.png)",
+        '5-img':"url(/img/5.png)",
+        '6-img':"url(/img/6.png)",
+        '7-img':"url(/img/7.png)",
+
+        '8-img':"url(/img1/11.png)",
+        '9-img':"url(/img2/31.png)",
+        '10-img':"url(/img2/32.png)",
+        '11-img':"url(/img2/33.png)",
+        '12-img':"url(/img2/34.png)",
+        '13-img':"url(/img2/35.png)",
+        '14-img':"url(/img2/36.png)",
+        '15-img':"url(/img2/37.png)",
+        '16-img':"url(/img2/38.png)",
+        '17-img':"url(/img2/39.png)",
+        '18-img':"url(/img2/40.png)",
+        '19-img':"url(/img2/41.png)",
+        '20-img':"url(/img2/42.png)",
+        '21-img':"url(/img2/43.png)",
+        '22-img':"url(/img2/44.png)",
+        '23-img':"url(/img3/45.png)",
+        '24-img':"url(/img3/46.png)",
+        '25-img':"url(/img3/47.png)",
+        '26-img':"url(/img3/48.png)",
+        '27-img':"url(/img3/49.png)",
+        '28-img':"url(/img3/50.png)",
+        '29-img':"url(/img3/51.png)",
+        '30-img':"url(/img3/52.png)",
+        '31-img':"url(/img3/53.png)",
+        '32-img':"url(/img3/54.png)",
+        '33-img':"url(/img3/55.png)",
+        '34-img':"url(/img3/56.png)",
+        '35-img':"url(/img3/57.png)",
+        '36-img':"url(/img3/58.png)",
+        '37-img':"url(/img4/1.png)",
+        '38-img':"url(/img4/2.png)",
+        '39-img':"url(/img4/3.png)",
+        '40-img':"url(/img4/4.png)",
+        '41-img':"url(/img4/5.png)",
+        '42-img':"url(/img4/6.png)",
+        '43-img':"url(/img4/7.png)",
+
+        
+
+
+      
+        
+      }),
+      height: {
+        '128': '32rem',
+    },
+  },
+  plugins: [],
+}
+}
